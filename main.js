95ae57247bf69bead1f9ce7ba6f44692b7c9749e
solexy79 <agboolasola6@gmail.com>
2020-07-01 03:31:25 +0100

    added an interactive greeting, works with real time

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+let greeting = new Date().getHours();
+console.log(greeting);
+
+if (greeting == 0 || greeting < 12){
+  greeting = 'morning';
+}else if(greeting == 12 || greeting < 17){
+  greeting = 'afternoon';
+}else if(greeting == 17 || greeting < 20){
+  greeting = 'evening';
+}else{
+  greeting = 'night';
+}
+
+
 // for the typing animation
-const heading = 'Hi Chandan, have a good evening!' ;
+const heading = 'Hi Chandan, have a good' + greeting +'!' ;
 let i = 0;
 
 const typing = () => {
